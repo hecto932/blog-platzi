8c697abd1a6a148d3b0cd281399b414ae0b1dea5
Hector Flores <hflores@nearsoft.comm>
2019-06-23 00:57:46 -0700

    Stateful vs Stateless

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,50 @@
-import React from "react";
+import React, { Component } from "react";
 
-const App = props => {
-  const createRows = () => [
-    <tr>
-      <td>Hector</td>
-      <td>hecto932@gmail.com</td>
-      <td>
-        <a href="https://github.com/hecto932">hectorLink</a>
-      </td>
-    </tr>,
-    <tr>
-      <td>Hector</td>
-      <td>hecto932@gmail.com</td>
-      <td>
-        <a href="https://github.com/hecto932">hectorLink</a>
-      </td>
-    </tr>
-  ];
-  return (
-    <div className="margin">
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Link</th>
-          </tr>
-        </thead>
-        <tbody>
-          {createRows()}
-        </tbody>
-      </table>
-    </div>
-  );
+class App extends Component{
+  constructor () {
+    super();
+    this.state = {
+      users: [
+        {
+          name: 'Hector',
+          email: 'hector@flores.com',
+          link: 'hectorflores.com'
+        },
+        {
+          name: 'Hector2',
+          email: 'hector2@flores.com',
+          link: 'hectorflores2.com'
+        }
+      ]
+    }
+  }
+  createRows = () => (
+    this.state.users.map(u => (
+      <tr>
+        <td>{u.name}</td>
+        <td>{u.email}</td>
+        <td>{u.link}</td>
+      </tr>
+    ))
+  )
+  render () {
+    return (
+      <div className="margin">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Link</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.createRows()}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
 };
 
 export default App;
