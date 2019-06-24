bcd7349a187e57397be4caecb247b46ba7a651db
Hector Flores <hflores@nearsoft.comm>
2019-06-24 12:46:50 -0700

    Share reducers

--- src/components/Publications/index.js
+++ src/components/Publications/index.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+import * as usersActions from '../../actions/usersActions'
 
 class Publications extends Component {
+  componentDidMount() {
+    const { users } = this.props
+    if (!users.length) {
+      this.props.getUsers()
+    }
+  }
   render () {
+    console.log(this.props)
     const { key } = this.props.match.params
     return (
       <div className="margin">
+        <h2>Publications</h2>
         { key }
       </div>
     )
   }
 }
 
-export default Publications
+const mapStateToProps = reducers => {
+  return reducers.usersReducer;
+}
+
+export default connect(mapStateToProps, usersActions)(Publications)
