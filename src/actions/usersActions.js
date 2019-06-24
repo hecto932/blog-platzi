74e84e32c89896193eb8c54a9a9f3158f9fe8fe1
Hector Flores <hflores@nearsoft.comm>
2019-06-23 23:10:22 -0700

    Asynchronous scenaries

--- src/actions/usersActions.js
+++ src/actions/usersActions.js
@@ -1,16 +1,16 @@
-import { FETCH_USERS } from '../types/usersTypes'
+import { FETCH_USERS } from '../types/usersTypes';
 
-export const getUsers = () => async (dispatch) => {
-  const response = await fetch('https://jsonplaceholder.typicode.com/users', { method: 'GET' })
-    const data = await response.json()
-    const users = data.map(u => ({
-      id: u.id,
-      name: u.name,
-      email: u.email,
-      website: u.website
-    }))
-  dispatch({
-    type: FETCH_USERS,
-    payload: users
-  })
-}
+export const getUsers = () => async dispatch => {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users', {
+      method: 'GET'
+    });
+    const users = await response.json();
+    dispatch({
+      type: FETCH_USERS,
+      payload: users
+    });
+  } catch (err) {
+    console.error(err.message)
+  }
+};
