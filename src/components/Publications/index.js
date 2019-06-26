6978e975bfad50b1b8092aa50088dc525495a8c2
Hector Flores <hflores@nearsoft.comm>
2019-06-26 10:58:40 -0700

    Avoid rewrite

--- src/components/Publications/index.js
+++ src/components/Publications/index.js
@@ -4,21 +4,52 @@ import { connect } from 'react-redux'
 import * as usersActions from '../../actions/usersActions'
 import * as publicationsActions from '../../actions/publicationsActions'
 
+import Spinner from '../General/Spinner'
+import Fatal from '../General/Fatal'
+
 class Publications extends Component {
   async componentDidMount() {
+    const { getUsers, getPublicationsByUser } = this.props
     const { key:userId } = this.props.match.params
-    const { users } = this.props.usersReducer
-    if (!users.length) {
-      await this.props.getUsers()
+    if (!this.props.usersReducer.users.length) {
+      await getUsers()
     }
-    this.props.getPublicationsByUser(userId)
+    if (this.props.usersReducer.error) {
+      return;
+    }
+    if (!('lastPublicationKey' in this.props.usersReducer.users[userId])) {
+      getPublicationsByUser(userId)
+    }
   }
+
+  handleCreateUser = () => {
+    const {
+      usersReducer,
+      match: { params: { key } }
+    } = this.props
+
+    if (usersReducer.error) {
+      return <Fatal message={usersReducer.error}/>
+    }
+
+    if (!usersReducer.users.length || usersReducer.isLoading) {
+      return <Spinner />
+    }
+
+    const { name } = usersReducer.users[key]
+
+    return (
+      <h1>{name} publications</h1>
+    )
+  }
+
+
   render () {
     console.log(this.props)
     const { key } = this.props.match.params
     return (
       <div className="margin">
-        <h2>Publications</h2>
+        { this.handleCreateUser() }
         { key }
       </div>
     )
